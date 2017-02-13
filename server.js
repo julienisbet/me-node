250c94e7e7d9d9f9a7378c03a871b4534dfa56e7
Julie Nisbet <jmnisbet@gmail.com>
2017-02-12 18:37:05 -0800

    heroku fixes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('dist'));
 app.use(express.static('public'));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
@@ -12,6 +14,6 @@ app.get('/*', function (req, res) {
   res.sendfile('./public/dist/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(app.get('port'), function () {
+  
 });
