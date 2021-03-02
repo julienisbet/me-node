dc64930f411494703776203d3b8ea411e46a9d5b
Julie Nisbet <jmnisbet@gmail.com>
2021-03-02 13:53:54 -0800

    using create-react-app

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('dist'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('dist'));
+  app.get(/\/(?!api)*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
 
 app.get('/resume', function(req, res) {
   res.sendFile('./dist/assets/resume.pdf')
 });
+
 app.get('/*', function (req, res) {
   res.sendFile('./dist/index.html');
 });
 
-app.listen(app.get('port'), function () {
-
-});
+app.listen(app.get('port'));
