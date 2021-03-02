47b021acaa82d916a5b3f51dedab2d9f5f82e601
Julie Nisbet <jmnisbet@gmail.com>
2021-03-01 21:01:33 -0800

    small bio update

--- client/components/Bio.jsx
+++ client/components/Bio.jsx
@@ -10,7 +10,7 @@ export default class Bio extends React.Component {
     return (
         <div className="bio">
             <h2>Web Developer since 2013, MBA since 2010, Data Analyst since 2004, American since 2003, Human since 1982.</h2>
-            <div>I value clean code, beautiful design and thorough testing. Full-stack experience includes Ruby/Rails, Python/Django, ReactJS, AngularJS, HTML/CSS plus a little NodeJS and Swift.</div>
+            <div>I value clean code, beautiful design and thorough testing. Full-stack experience includes Ruby/Rails, Python/Django, NodeJS, React, HTML/CSS.</div>
         </div>
     )
   }
