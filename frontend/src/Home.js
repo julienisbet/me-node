dce2f54f53a674130b37e316d058d7a2b4b0fdce
Julie Nisbet <jmnisbet@gmail.com>
2021-03-03 13:15:17 -0800

    cleanup

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,12 +6,12 @@ function Home(){
             <div className="headline">
                 <h1>Julie Nisbet</h1>
                 <div id="links">
-                    <a href="https://github.com/julienisbet" target="_blank"><img src="logos/github.png"/></a>
-                    <a href="http://www.linkedin.com/in/julienisbet" target="_blank"><img src="logos/linkedin.png"/></a>
-                    <a href="http://julesnisbet.tumblr.com/" target="_blank"><img src="logos/tumblr.png"/></a>
-                    <a href="http://www.instagram.com/juleslaroo" target="_blank"><img src="logos/instagram.png"/></a>
-                    <a href="http://www.twitter.com/julesnisbet" target="_blank"><img src="logos/twitter.png"/></a>
-                    <Link to="/resume" target="_blank"><img src="logos/resume.png"/></Link>
+                    <a href="https://github.com/julienisbet" target="_blank" rel="noreferrer"><img src="logos/github.png"/></a>
+                    <a href="http://www.linkedin.com/in/julienisbet" target="_blank" rel="noreferrer"><img src="logos/linkedin.png"/></a>
+                    <a href="http://julesnisbet.tumblr.com/" target="_blank" rel="noreferrer"><img src="logos/tumblr.png"/></a>
+                    <a href="http://www.instagram.com/juleslaroo" target="_blank" rel="noreferrer"><img src="logos/instagram.png"/></a>
+                    <a href="http://www.twitter.com/julesnisbet" target="_blank" rel="noreferrer"><img src="logos/twitter.png"/></a>
+                    <Link to="/resume" target="_blank" rel="noreferrer"><img src="logos/resume.png"/></Link>
                 </div>
             </div>
             <div className="about">
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/resume', function(req, res) {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "resume.pdf"));
+});
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('dist'));
+  app.use(express.static('frontend/build'));
   app.get(/\/(?!api)*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
+
+
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
-
-app.get('/resume', function(req, res) {
-  res.sendFile('./dist/assets/resume.pdf')
+app.listen(app.get('port'), () => {
+  console.log("listening...")
 });
-
-app.get('/*', function (req, res) {
-  res.sendFile('./dist/index.html');
-});
-
-app.listen(app.get('port'));
